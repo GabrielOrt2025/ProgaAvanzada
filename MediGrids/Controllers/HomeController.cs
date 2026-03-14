3ebd55b2e3092ea65c898915fb79b780dc74ef61
fabi-zamorao <bolitafabi@gmail.com>
2026-03-14 00:49:09 -0600

    Implementación de módulo Biblioteca de Ejercicios con filtros y layout interno
    Se implementa la vista Biblioteca de Ejercicios dentro del layout interno del sistema.
    
    Incluye:
    - Integración con Entity Framework para listar ejercicios.
    - Filtro por Tipo de Terapia y Categoría Clínica.
    - Uso de ViewBag para cargar terapias y categorías desde la base de datos.
    - Layout interno con menú de navegación para módulos autenticados.
    - Botón para visualizar video de cada ejercicio.

--- MediGrids/Controllers/HomeController.cs
+++ MediGrids/Controllers/HomeController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MediGrids.EntityFramework;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MediGrids.Controllers
 {
     public class HomeController : Controller
     {
+        private Entities db = new Entities();
+
         public ActionResult Index()
         {
             return View();
@@ -17,12 +18,42 @@ namespace MediGrids.Controllers
         {
             return View();
         }
+
         public ActionResult Login()
         {
-
             return View();
         }
 
-      
+        public ActionResult BibliotecaEjercicios(int? idTerapia, int? idCategoria)
+        {
+            var ejercicios = db.Ejercicio
+                .Include(e => e.CategoriaClinica)
+                .AsQueryable();
+
+            ViewBag.Terapias = db.TipoTerapia.ToList();
+
+            if (idTerapia.HasValue)
+            {
+                ViewBag.Categorias = db.CategoriaClinica
+                    .Where(c => c.id_terapia == idTerapia.Value)
+                    .ToList();
+
+                ejercicios = ejercicios.Where(e => e.id_terapia == idTerapia.Value);
+            }
+            else
+            {
+                ViewBag.Categorias = db.CategoriaClinica.ToList();
+            }
+
+            if (idCategoria.HasValue)
+            {
+                ejercicios = ejercicios.Where(e => e.id_categoria == idCategoria.Value);
+            }
+
+            ViewBag.IdTerapia = idTerapia;
+            ViewBag.IdCategoria = idCategoria;
+
+            return View(ejercicios.ToList());
+        }
     }
 }
