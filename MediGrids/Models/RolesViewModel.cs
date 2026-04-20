c9d917e17f21e72f2a3c5a892e77735fd81a1b0c
GabrielOrt2025 <gortega00053@ufide.ac.cr>
2026-04-19 22:23:10 -0600

    Modules de gestion de pacientes y roles

---
+++ MediGrids/Models/RolesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MediGrids.Models
+{
+    public class RolesViewModel
+    {
+        public int id_usuario { get; set; }
+        public string correo { get; set; }
+        public int id_rol { get; set; }
+        public string nombre_rol { get; set; }
+        public bool? activo { get; set; }
+    }
+}
